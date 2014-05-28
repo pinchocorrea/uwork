db3b3e74b944fc561cbd46b3fe8da6c74d770f1e
pinchocorrea <pinchocorrea@gmail.com>
2014-05-28 13:13:23 -0400

    Arreglo students model

---
+++ app/models/student.rb
@@ -0,0 +1,12 @@
+class Student < ActiveRecord::Base
+
+    validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+    def self.to_csv(all_students, options={})
+        CSV.generate(options) do |csv|
+            csv << column_names
+            all_students.each do |student|
+                csv << student.attributes.values_at(*column_names)
+            end
+        end
+    end
+end
