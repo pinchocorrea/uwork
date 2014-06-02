d6a38e18eca2beec4a8769ca47247f91d4fda218
pinchocorrea <pinchocorrea@gmail.com>
2014-06-02 16:49:41 -0400

    postula path

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -23,7 +23,7 @@ class StudentsController < ApplicationController
 	    @student = Student.find(params[:id])
 
 		if @student.update(student_params)
-			redirect_to students_show_path
+			redirect_to postula_path
 		else
 			render 'edit'
 		end
